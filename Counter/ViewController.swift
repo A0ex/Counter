ef9ca5a684269f359fbb155599903ffddf300693
A0ex <04aas@mail.ru>
2023-12-13 22:25:13 +0300

    Добавлена кнопка для подсчета кликов и счетчик (UILabel)

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -8,10 +8,17 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    private var clickCounter = 0
+    
+    @IBOutlet weak var counterLabel: UILabel!
+    
+    @IBAction func counterButton(_ sender: Any) {
+        clickCounter += 1
+        counterLabel.text = "Значение счётчика: \(clickCounter)"
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
 
 
