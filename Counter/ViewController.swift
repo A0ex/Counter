9bcd31e62c9834eed265daed32e2be94572123ab
A0ex <alxslvrstv@yandex.ru>
2023-12-18 20:09:14 +0300

    Добавлено текстовое поле UITextView, в котором отражается история изменения значения счётчика

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -15,27 +15,58 @@ class ViewController: UIViewController {
     
     @IBAction func counterAddButton(_ sender: Any) {
         feedbackGenerator.impactOccurred()
+        let range = NSMakeRange(infoTextView.text.count - 1, 1)
         clickCounter += 1
         counterLabel.text = "Значение счётчика: \(clickCounter)"
+        
+        let currentDate = Date()
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "dd.MM.yyyy HH:mm:ss: значение изменено на +1\n"
+        infoTextView.text.append(dateFormatter.string(from: currentDate))
+        
+        infoTextView.scrollRangeToVisible(range)
+        
+        
     }
     @IBAction func counterSubButton(_ sender: Any) {
         feedbackGenerator.impactOccurred()
+        let range = NSMakeRange(infoTextView.text.count - 1, 1)
+        let currentDate = Date()
+        let dateFormatter = DateFormatter()
         if clickCounter != 0 {
             clickCounter -= 1
             counterLabel.text = "Значение счётчика: \(clickCounter)"
+            dateFormatter.dateFormat = "dd.MM.yyyy HH:mm:ss: значение изменено на -1\n"
+            infoTextView.text.append(dateFormatter.string(from: currentDate))
+            infoTextView.scrollRangeToVisible(range)
+        } else {
+            dateFormatter.dateFormat = "dd.MM.yyyy HH:mm:ss: попытка уменьшить значение счётчика ниже 0\n"
+            infoTextView.text.append(dateFormatter.string(from: currentDate))
+            infoTextView.scrollRangeToVisible(range)
         }
     }
     
     @IBAction func resetButton(_ sender: Any) {
         feedbackGenerator.impactOccurred()
+        let range = NSMakeRange(infoTextView.text.count - 1, 1)
         clickCounter = 0
         counterLabel.text = "Значение счётчика: \(clickCounter)"
+        
+        let currentDate = Date()
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "dd.MM.yyyy HH:mm:ss: значение сброшено\n"
+        infoTextView.text.append(dateFormatter.string(from: currentDate))
+        infoTextView.scrollRangeToVisible(range)
     }
+    
+    @IBOutlet weak var infoTextView: UITextView!
     override func viewDidLoad() {
         super.viewDidLoad()
+        infoTextView.isEditable = false
+        infoTextView.text.append("\n")
         
     }
 
-
+    
 }
 
